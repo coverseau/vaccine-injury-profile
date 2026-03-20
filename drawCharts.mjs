e1965b2a932d24bbee3e36f2f457e3a42b1034ef
Rado Faletič <radofaletic@gmail.com>
2026-03-20 18:08:26 +1100

    custom colours for states

--- drawCharts.mjs
+++ drawCharts.mjs
@@ -1,21 +1,19 @@
 
 import { Chart, ArcElement, Legend, PieController, Tooltip } from 'https://cdn.jsdelivr.net/npm/chart.js/+esm';
-import DataLabels from 'https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels/+esm';
-import AutoColors from 'https://cdn.jsdelivr.net/npm/chartjs-plugin-autocolors/+esm';
+//import DataLabels from 'https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels/+esm';
+//import AutoColors from 'https://cdn.jsdelivr.net/npm/chartjs-plugin-autocolors/+esm';
 
 import summaryData from './data.mjs';
 
 document.addEventListener("DOMContentLoaded", async () => {
 	Chart.register([
 		ArcElement,
-		AutoColors,
-		DataLabels,
+		//AutoColors,
+		//DataLabels,
 		Legend,
 		PieController,
 		Tooltip
 	]);
-	Chart.defaults.plugins.autocolors.enabled = true;
-	Chart.defaults.plugins.legend.display = true;
 	Chart.defaults.plugins.tooltip.enabled = true;
 	drawStates('figure_states', summaryData.demographics.state);
 });
@@ -27,13 +25,40 @@ function drawStates(id, states) {
 			labels: [],
 			datasets: [{
 				label: 'State',
-				data: []
+				data: [],
+				backgroundColor: []
 			}]
 		};
 		Object.keys(states).forEach(state => {
 			if (state != 'N') {
 				data.labels.push(state);
 				(data.datasets)[0].data.push(states[state]);
+				switch (state) {
+					case 'ACT':
+						(data.datasets)[0].backgroundColor.push('#003dA5');
+						break;
+					case 'NSW':
+						(data.datasets)[0].backgroundColor.push('#9bcbeb');
+						break;
+					case 'NT':
+						(data.datasets)[0].backgroundColor.push('#c25e03');
+						break;
+					case 'QLD':
+						(data.datasets)[0].backgroundColor.push('#73182c');
+						break;
+					case 'SA':
+						(data.datasets)[0].backgroundColor.push('#d50032');
+						break;
+					case 'TAS':
+						(data.datasets)[0].backgroundColor.push('#006747');
+						break;
+					case 'VIC':
+						(data.datasets)[0].backgroundColor.push('#003c71');
+						break;
+					case 'WA':
+						(data.datasets)[0].backgroundColor.push('#ffd100');
+						break;
+				}
 			}
 		});
 		new Chart(
@@ -43,14 +68,14 @@ function drawStates(id, states) {
 				data: data,
 				options: {
 					plugins: {
-						autocolors: {
-							mode: 'data'
-						}/*,
+						legend: {
+							display: true;
+						},
 						datalabels: {
 							formatter: (value, context) => {
 								return (100.0 * value).toFixed(1) + '%';
 							}
-						}*/
+						}
 					}
 				}
 			}
