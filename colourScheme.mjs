134bab7ec7a8771532346da2be46257c6b8a5768
Rado Faletič <radofaletic@gmail.com>
2026-03-20 21:21:09 +1100

    changed some things

--- colourScheme.mjs
+++ colourScheme.mjs
@@ -1,28 +1,26 @@
 
-document.addEventListener("DOMContentLoaded", async () => {
-	const colourTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : (window.matchMedia('(prefers-color-scheme: light)').matches ? 'light' : 'auto');
+const colourTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : (window.matchMedia('(prefers-color-scheme: light)').matches ? 'light' : 'auto');
+
+document.documentElement.setAttribute('data-bs-theme', colourTheme);
+
+const urlQuery = window.location.search;
+
+const urlQueryParams = new URLSearchParams(urlQuery);
+
+const overrideTheme = urlQueryParams.get('theme');
+
+if (!!overrideTheme) {
 	
-	document.documentElement.setAttribute('data-bs-theme', colourTheme);
-	
-	const urlQuery = window.location.search;
-	
-	const urlQueryParams = new URLSearchParams(urlQuery);
-	
-	const overrideTheme = urlQueryParams.get('theme');
-	
-	if (!!overrideTheme) {
-		
-		switch (overrideTheme) {
-				
-			case 'dark':
-				document.documentElement.setAttribute('data-bs-theme', 'dark');
-				break;
-				
-			case 'light':
-				document.documentElement.setAttribute('data-bs-theme', 'light');
-				break;
-				
-		}
-		
+	switch (overrideTheme) {
+			
+		case 'dark':
+			document.documentElement.setAttribute('data-bs-theme', 'dark');
+			break;
+			
+		case 'light':
+			document.documentElement.setAttribute('data-bs-theme', 'light');
+			break;
+			
 	}
-});
+	
+}
